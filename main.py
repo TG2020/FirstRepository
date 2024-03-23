138f7565ff7041ba53aaebb87a0d31f36b3028f0
Tomasz Graczyk <tgraczyk86@gmail.com>
2024-03-23 12:47:03 +0100

    Init project

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ for i in zakupy1.keys():
     for n in zakupy1.values():
         print("Idę do", i.title(), "kupuję tu :", str(n).title())
 
+for i in zakupy2.keys():
+    for n in zakupy2.values():
+        print("Idę do", i.title(), "kupuję tu :", str(n).title())
+
+shopping_list = ["marchew", "seler", "rukola", "chleb", "pączek", "bułka"]
+print("W sumie kupię ", len(shopping_list), "produktów")
